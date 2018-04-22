e6557d8381ff14c1124beebc88e7ccb97fd69525
Reuben Thomas <rrt@sc3d.org>
2018-04-22 16:00:36 +0100

    Add proper license headers to source files

--- src/tbl_commands.h
+++ src/tbl_commands.h
@@ -1,3 +1,13 @@
+// List of Beetle debugger commands.
+//
+// (c) Reuben Thomas 2018
+//
+// The package is distributed under the GNU Public License version 3, or,
+// at your option, any later version.
+//
+// THIS PROGRAM IS PROVIDED AS IS, WITH NO WARRANTY. USE IS AT THE USER‘S
+// RISK.
+
 C(TOD)
 C(TOR)
 C(COUNTS)
--- src/tbl_registers.h
+++ src/tbl_registers.h
@@ -1,3 +1,13 @@
+// List of Beetle register names.
+//
+// (c) Reuben Thomas 2018
+//
+// The package is distributed under the GNU Public License version 3, or,
+// at your option, any later version.
+//
+// THIS PROGRAM IS PROVIDED AS IS, WITH NO WARRANTY. USE IS AT THE USER‘S
+// RISK.
+
 R(A)
 R(NOT_ADDRESS)
 R(BAD)
