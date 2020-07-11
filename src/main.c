492b7cd02f657740dd67de571e28b0263ff9d550
Reuben Thomas <rrt@sc3d.org>
2020-07-11 13:47:47 +0100

    src/main.c: fix a couple of memory leaks in usage()
    These are transient (because usage() is only called just before exit).

--- src/main.c
+++ src/main.c
@@ -816,10 +816,12 @@ static void usage(void)
             "Run Beetle.\n"
             "\n",
             program_name);
-#define OPT(longname, shortname, arg, argstring, docstring)               \
+#define OPT(longname, shortname, arg, argstring, docstring)             \
     shortopt = xasprintf(", -%c", shortname);                           \
     buf = xasprintf("--%s%s %s", longname, shortname ? shortopt : "", argstring); \
-    printf("  %-26s%s\n", buf, docstring);
+    printf("  %-26s%s\n", buf, docstring);                              \
+    free(shortopt);                                                     \
+    free(buf);
 #define ARG(argstring, docstring)                 \
     printf("  %-26s%s\n", argstring, docstring);
 #define DOC(text)                                 \
